5613b93b72248406611904d83dd4e1b517961d86
Mission Bit <missionbit@Missions-MacBook-Pro.local>
2020-10-21 17:28:26 -0700

    turtle behind line

--- race_game.py
+++ race_game.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+game = True
 
 player1 = turtle.Turtle()
 player2 = turtle.Turtle()
@@ -8,7 +9,6 @@ lines = turtle.Turtle()
 lines.penup()
 lines.setposition(0,300)
 lines.pendown()
-lines.write("--Turtle Graphics Race--",font=("Calibri", 30, "bold"))
 lines.speed(100)
 lines.penup()
 lines.setposition(-250,-300)
@@ -22,5 +22,20 @@ lines.forward(650)
 
 player1.shape("turtle")
 player1.color("red")
+player1.penup()
+player1.setposition(-265,-150)
+
+player2.shape("turtle")
+player2.color("blue")
+player2.penup()
+player2.setposition(-265,150)
+
+move1 = [20,50,60,0,90,-5,-10,-50,100,10]
+move2 = [20,50,60,0,90,-5,-10,-50,100,10]
+
+    
+    
+
+
 
 turtle.mainloop()
